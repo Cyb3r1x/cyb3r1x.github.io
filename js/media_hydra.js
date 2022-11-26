73f2d6892643f82f1553be99226d455ccfff566d
Cyb3r1x <118626342+Cyb3r1x@users.noreply.github.com>
2022-11-25 21:33:25 -0600

    add: hydra scripts

--- js/media_hydra.js
+++ js/media_hydra.js
@@ -15,6 +15,8 @@ hydraCanvas.height = window.innerHeight;
 
 /////////////////////////////////////////////////////
 
+let code = null;
+
 hydraCanvas2.width = window.innerWidth;
 hydraCanvas2.height = window.innerHeight;
 
@@ -27,15 +29,44 @@ const cam_instance = new Hydra({
 }).synth
 
 cam_switch.addEventListener('click', ()=>{
-  cam_audio.play();
-  cam_instance.s1.initCam();
-  const s1 = cam_instance.s1;
+  if(code == null){
+    cam_audio.play();
+    cam_instance.s1.initCam();
+  }
+  
+  code += 1;
 
-  cam_instance.shape(100,0.4,0.8)
-  .blend(cam_instance.o0,0.5)
-  .modulate(cam_instance.noise(2,0.2))
-  .diff(cam_instance.s1,0.5)
-  .out();
+  if(code >= 5){
+    code = 1;
+  }
+
+  if(code == 1){
+    cam_instance.shape(100,0.4,0.8)
+    .blend(cam_instance.o0,0.5)
+    .modulate(cam_instance.noise(2,0.2))
+    .diff(cam_instance.s1,0.5)
+    .out();
+  } else if (code == 2) {
+    cam_instance.noise(2,0.5)
+    .modulateScale(cam_instance.s1,1)
+    .modulate(cam_instance.o0,0.8)
+    .diff(cam_instance.s1,[0.5,0.2,0.4,0.7,0.9,1.2].fast(5))
+    .out()
+  } else if (code == 3) {
+    cam_instance.voronoi()
+    .modulateRotate(cam_instance.s1,2)
+    .diff(cam_instance.o0,0.2)
+    .add(cam_instance.s1,0.5)
+    .colorama(0.05)
+    .out()
+  } else if (code == 4) {
+    cam_instance.shape(100,0.4,1)
+    .mult(cam_instance.s1,0.5)
+    .contrast(1.5)
+    .modulate(cam_instance.osc(10,0.1))
+    .rotate(2,0.1)
+    .out()
+  }
 });
 
 /////////////////////////////////////////////////////
